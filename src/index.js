7ea17bc0299a794473a12078e137fd3b4fcad9a8
Abas Muzi <106030034+AbasPortfolio@users.noreply.github.com>
2025-04-24 13:20:35 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/dist")));
+  app.use(express.static(path.join(__dirname, "dist")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "dist", "index.html"));
