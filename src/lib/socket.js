96b169df620b15be94ea8fa1349a3129022adc3a
Abas Muzi <106030034+AbasPortfolio@users.noreply.github.com>
2025-04-25 10:39:27 +0200

    Update socket.js

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://chat-quick-app.onrender.com",
+    origin: ["https://chat-quick-app.onrender.com"],
   },
 });
 
